92bc8fb49d9eff947c06874170a7dab3ab805c4c
James Rowley <james@jrowley.me>
2016-08-11 19:59:45 -0700

    Rename 'card' as 'leaf'
    It fits better with the analogy of the tree having leaves and branches.

--- js/rt-drag.js
+++ js/rt-drag.js
@@ -1,6 +1,6 @@
 ////////////////////Initialization
 $(function(){
 	//////////Handlers - Interface Niceties
-	//Grow card description field with contents.
-	$('.rt-card-description').on('input', function(){$(this).css({'height':'auto'}).height(this.scrollHeight > 107 ? this.scrollHeight : 107);});
+	//Grow leaf node description field with contents.
+	$('.rt-leaf-description').on('input', function(){$(this).css({'height':'auto'}).height(this.scrollHeight > 107 ? this.scrollHeight : 107);});
 });
