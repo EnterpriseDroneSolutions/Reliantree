2f86971eed4f567509a960a5aabd55b0c5612745
James Rowley <james@jrowley.me>
2016-08-12 09:37:37 -0700

    Rename 'leaf' as 'node'
    This corresponds to an internal requirement change.

--- js/rt-drag.js
+++ js/rt-drag.js
@@ -1,6 +1,6 @@
 ////////////////////Initialization
 $(function(){
 	//////////Handlers - Interface Niceties
-	//Grow leaf node description field with contents.
-	$('.rt-leaf-description').on('input', function(){$(this).css({'height':'auto'}).height(this.scrollHeight > 107 ? this.scrollHeight : 107);});
+	//Grow node description field with contents.
+	$('.rt-node-description').on('input', function(){$(this).css({'height':'auto'}).height(this.scrollHeight > 107 ? this.scrollHeight : 107);});
 });
